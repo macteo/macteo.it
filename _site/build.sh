4fbda99269e67cc1e6fae18bd30dc4cf85493841
Matteo Gavagnin <m@macteo.it>
2019-02-13 17:39:22 +0100

    Updated gems

--- _site/build.sh
+++ _site/build.sh
@@ -1,5 +1,5 @@
-sudo systemctl stop jekyll-drafts.service
-sudo bundle exec jekyll build
+# systemctl stop jekyll-drafts.service
+bundle exec jekyll build
 git add .
 MESSAGE="$1"
 if [ $1 -eq 0 ]
@@ -9,4 +9,4 @@ fi
 git commit -m "$MESSAGE"
 git push origin master
 cp -R _site ../macteo.it
-sudo systemctl start jekyll-drafts.service
+# systemctl start jekyll-drafts.service
