f7a8b479813bc399825ba8a984280907c3fac78b
Matteo Gavagnin <matteo@MacBook-Pro-di-dimension.local>
2021-06-24 11:00:21 +0200

    removed watch folder

--- watch/deploy.sh
+++
@@ -1,3 +0,0 @@
-cd $HOME/Library/Mobile\ Documents/com\~apple\~CloudDocs/macteo.it/
-bundle exec jekyll build
-rsync -az --delete-after $HOME/Library/Mobile\ Documents/com\~apple\~CloudDocs/macteo.it/* ovh.macteo.it:~/web/macteo.it/
